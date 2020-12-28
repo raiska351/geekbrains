57ef5e40551f71cba0ada0970aebe9dc2c5f4aa5
raiska <raisa-translation@yandex.ru>
2020-12-28 13:01:48 +0300

    домашка

---
+++ homeworkLesson4/homeworkLesson4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  homeworkLesson4
+//
+//  Created by RAISA on 28.12.2020.
+//  Copyright © 2020 RAISA. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
